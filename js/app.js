45863ad07b0ecb22340006564566ef9dff0077ab
Joao Marcos <joaomarcos11.tech@gmail.com>
2021-01-19 16:33:58 -0300

    JQuery: menu com scroll e link ativo

--- js/app.js
+++ js/app.js
@@ -1,2 +1,37 @@
-// $(body).addClass
-console.log("Olá mundo");
+// Scroll no window
+$(window).scroll(() => {
+  let menuTopOffset = $("#nav-side").offset().top;
+
+  const inicioTopOffset = $("#inicio").offset().top;
+  const inicioHeight = $("#inicio").height();
+
+  const faixaTopOffset = $("#faixa").offset().top;
+  const faixaHeight = $("#faixa").height();
+
+  const utilizarTopOffset = $("#utilizar").offset().top;
+  const utilizarHeight = $("#utilizar").height();
+
+  const pesquisaTopOffset = $("#pesquisa").offset().top;
+  const pesquisaHeight = $("#pesquisa").height();
+
+  $("#nav-side").offset({
+    top: ($(this).scrollTop() + 40)
+  });
+
+  if(menuTopOffset > faixaTopOffset) {
+    $('#nav-side a[href^="#faixa"]').addClass('anchor-active');
+  }
+  if(menuTopOffset > (faixaTopOffset + faixaHeight)) {
+    $('#nav-side a[href^="#faixa"]').removeClass('anchor-active');
+  }
+  if(menuTopOffset < faixaTopOffset) {
+    $('#nav-side a[href^="#faixa"]').removeClass('anchor-active');
+  }
+
+  // console.log(`Menu: ${menuTopOffset}`);
+  // console.log(`Faixa: ${faixaTopOffset}`);
+});
+
+
+
+
